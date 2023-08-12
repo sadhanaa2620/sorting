6e17c3d2e8049a61e43420d2caeba25bdc72bc63
sadhanaa2620 <86036901+sadhanaa2620@users.noreply.github.com>
2023-08-12 14:42:14 +0530

    Add files via upload
    Write a program to implement Quick sort algorithm for sorting the elements of an array.
    Input Format:
    
    The first line corresponds to size of an array.
    
    The second line corresponds to the elements in the array.
    
    Output Format:
    
    Sort an array using Quick sort algorithm and print sorted array.
    
    Sample Input:
    
    6
    
    11 15 26 38 9 10 
    
    Sample Output:
    
    9 10 11 15 26 38

--- Main.java
+++ Main.java
@@ -1,17 +1,51 @@
-import java.util.*;
-class Main
-{
-  public static void main(String args[])
-  {
-    Scanner sc = new Scanner(System.in);
-    int count = 0;
-    int n = sc.nextInt();
-    int arr[] = new int[n];
-    for(int i=0;i<n;i++){
-      arr[i] = sc.nextInt();
-      if(arr[i]==1)
-        count++;
+import java.util.Scanner;
+
+public class Main {
+   
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        int[] arr = new int[n];
+       
+        for (int i = 0; i < n; i++) {
+            arr[i] = scanner.nextInt();
+        }
+       
+        quickSort(arr, 0, n - 1);
+       
+        for (int num : arr) {
+            System.out.print(num + " ");
+        }
+       
+        scanner.close();
     }
-    System.out.println("Count of 1's in given array is "+count);
-  }
+   
+    public static void quickSort(int[] arr, int low, int high) {
+        if (low < high) {
+            int pivotIndex = partition(arr, low, high);
+           
+            quickSort(arr, low, pivotIndex - 1);
+            quickSort(arr, pivotIndex + 1, high);
+        }
+    }
+   
+    public static int partition(int[] arr, int low, int high) {
+        int pivot = arr[high];
+        int i = low - 1;
+       
+        for (int j = low; j < high; j++) {
+            if (arr[j] < pivot) {
+                i++;
+                int temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+            }
+        }
+       
+        int temp = arr[i + 1];
+        arr[i + 1] = arr[high];
+        arr[high] = temp;
+       
+        return i + 1;
+    }
 }
